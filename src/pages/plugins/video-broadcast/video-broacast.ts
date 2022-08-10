d0226e192b349157881d4be85b722a5a986bfc16
wzp123321 <wanzhipengx@163.com>
2022-08-10 22:15:10 +0800

    新增语音播放

--- src/pages/plugins/video-broadcast/video-broacast.ts
+++ src/pages/plugins/video-broadcast/video-broacast.ts
@@ -14,7 +14,7 @@ export default defineComponent({
     const videoBroadCast = async () => {
       const mp3 =
         'https://tts.baidu.com/text2audio?cuid=baike&lan=zh&ctp=1&pdt=301&vol=9&rate=32&per=0&tex=' +
-        encodeURI('测出猜测猜测测出猜测猜测测出猜测猜测测出猜测猜测测出猜测猜测测出猜测猜测');
+        encodeURI('支付宝到账1亿元》》》');
       const musicArrayBuffer = await getMp3ArrayBuffer(mp3);
       const decodedAudioData = await decode(musicArrayBuffer);
       play(decodedAudioData);
@@ -35,7 +35,7 @@ export default defineComponent({
      * @returns {ArrayBuffer}
      */
     const getMp3ArrayBuffer = async (url: string) => {
-      return fetch(url).then((r) => r.arrayBuffer());
+      return fetch(url).then(r => r.arrayBuffer());
     };
 
     /**
@@ -45,8 +45,22 @@ export default defineComponent({
     const decode = async (arrayBuffer: any) => {
       return audioContext.decodeAudioData(arrayBuffer);
     };
+
+    const speak = () => {
+      const speechSynthesis = window.speechSynthesis;
+      const speechUtterance = new SpeechSynthesisUtterance('支付宝到账1万元');
+      // speechUtterance.text = '支付宝到账1万元'; // 文字内容
+      // speechUtterance.lang = 'zh-CN'; // 使用的语言:中文-zh-CN
+      // speechUtterance.volume = 1; // 声音音量：1
+      // speechUtterance.rate = 0.75; // 语速：1
+      // speechUtterance.pitch = 1; // 音高：1
+      speechSynthesis.speak(speechUtterance); // 这个播放是重复播放
+      // speechSynthesis.speak(speechUtterance); //这个是不重复播放
+    };
+
     return {
       videoBroadCast,
+      speak,
     };
   },
 });
