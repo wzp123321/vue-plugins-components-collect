cb0f234c010c37adb3fad2ad4e0838e55624294f
wzp123321 <wanzhipengx@163.com>
2022-08-13 23:14:15 +0800

    --user=wanzhipeng 新增加密算法

---
+++ src/pages/plugins/autoheight-textarea/autoheight-textarea.vue
@@ -0,0 +1,3 @@
+<template src="./autoheight-textarea.html"></template>
+<script lang="ts" src="./autoheight-textarea"></script>
+<style lang="less" scoped src="./autoheight-textarea.less"></style>
--- src/router/modules/plugins.ts
+++ src/router/modules/plugins.ts
@@ -15,6 +15,13 @@ const pluginRoutes: RouteRecordRaw[] = [
     },
     component: () => import('../../pages/plugins/crypto-js/crypto-js.vue'),
   },
+  {
+    path: '/plugins-textarea',
+    meta: {
+      name: '文本框自适应',
+    },
+    component: () => import('../../pages/plugins/autoheight-textarea/autoheight-textarea.vue'),
+  },
 ];
 
 export default pluginRoutes;
