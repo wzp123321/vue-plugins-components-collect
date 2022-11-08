f55730cd2f32e2fa79125c72031472a116cededd
wanzp <wanzp@tiansu-china.com>
2022-11-08 19:32:05 +0800

    新增地图背景

--- src/pages/echarts/chart-map/chart-map.ts
+++ src/pages/echarts/chart-map/chart-map.ts
@@ -5,21 +5,52 @@
  * @Last Modified by: zpwan
  * @Last Modified time: 2022-06-06 16:47:36
  */
-import { defineComponent, onMounted } from 'vue';
-
-import ChartMapService from './chart-map.service';
+import { defineComponent, onMounted, onUnmounted, ref } from 'vue'
+import { interval, Subject, takeUntil, timer } from 'rxjs'
+import ChartMapService from './chart-map.service'
 
 export default defineComponent({
   name: 'ChartMap',
   setup() {
-    const chartMapService = new ChartMapService();
+    const chartMapService = new ChartMapService()
+    const _destroy$ = new Subject<void>()
 
-    onMounted(() => {
-      chartMapService.initCharts();
-    });
+    const showContainer = ref<boolean>(false)
+    const showMap = ref<boolean>(false)
+    const elLoopVideo = ref<HTMLVideoElement>()
+
+    function startAnimation(event: Event) {
+      timer(2000)
+        .pipe(takeUntil(_destroy$))
+        .subscribe(() => (showContainer.value = true))
+      timer(4000)
+        .pipe(takeUntil(_destroy$))
+        .subscribe(() => {
+          showMap.value = true
+          chartMapService.initCharts()
+          ;(event.target as HTMLVideoElement)?.remove()
+          elLoopVideo.value?.classList.remove('ghost')
+          elLoopVideo.value?.play()
+        })
+    }
+
+    // onMounted(() => {
+    //   chartMapService.initCharts()
+    // })
+
+    onUnmounted(() => {
+      // 销毁订阅
+      _destroy$.next()
+      _destroy$.complete()
+    })
 
     return {
       chartMapService,
-    };
+      showContainer,
+      showMap,
+      elLoopVideo,
+
+      startAnimation,
+    }
   },
-});
+})
