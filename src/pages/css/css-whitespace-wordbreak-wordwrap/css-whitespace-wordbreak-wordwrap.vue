1e593a595a841b10f4ee16577e524ebc24578b81
wanzp <wanzp@tiansu-china.com>
2023-02-23 15:05:21 +0800

    新增css中处理换行、拆分单词的样式

---
+++ src/pages/css/css-whitespace-wordbreak-wordwrap/css-whitespace-wordbreak-wordwrap.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="css-whitespace-wordbreak-wordwrap" id="css-whitespace-wordbreak-wordwrap">
+    <div class="cwww-whitespace">
+      <h1>cwww-whitespace</h1>
+      <div class="cwww-whitespace-container">
+        <div class="item">{{ nwrapStr }}</div>
+        <div class="item nowrap">{{ nwrapStr }}</div>
+        <div class="item pre">{{ nwrapStr }}</div>
+        <div class="item prewrap">{{ nwrapStr }}</div>
+        <div class="item preline">{{ nwrapStr }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+const nwrapStr =
+  'This      is a incomprehensibilities long word.我在测\n试 我在    测试&nbsp;&nbsp;&nbsp;&nbsp;我n在测试&nbsp;&nbsp;&nbsp;我在测试br生效<br />在测试我在测试&nbsp;&nbsp;&nbsp;&nbsp;我在测';
+</script>
+<style lang="less" scoped>
+.css-whitespace-wordbreak-wordwrap {
+  width: 100%;
+  height: 100%;
+
+  .cwww-whitespace-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    row-gap: 24px;
+
+    .item {
+      width: 200px;
+      border: 1px solid rgba(0, 0, 0, 0.06);
+    }
+
+    .nowrap {
+      white-space: nowrap;
+    }
+
+    .pre {
+      white-space: pre;
+    }
+
+    .prewrap {
+      white-space: pre-wrap;
+    }
+
+    .preline {
+      white-space: pre-line;
+    }
+  }
+}
+</style>
--- src/router/modules/css.ts
+++ src/router/modules/css.ts
@@ -8,6 +8,13 @@ const cssComponents: RouteRecordRaw[] = [
     },
     component: () => import('../../pages/css/css-flex/cf-dice.vue'),
   },
+  {
+    path: '/css-whitespace-wordbreak-wordwrap',
+    meta: {
+      name: 'CSS-换行处理',
+    },
+    component: () => import('../../pages/css/css-whitespace-wordbreak-wordwrap/css-whitespace-wordbreak-wordwrap.vue'),
+  },
 ];
 
 export default cssComponents;
