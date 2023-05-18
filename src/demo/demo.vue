a111cc8fa2f5ac39bf9b38d482bb61aef3811cb7
wzp123321 <wanzhipengx@163.com>
2023-05-18 22:27:29 +0800

    --user=万直鹏 新增新过滤指令

--- src/demo/demo.vue
+++ src/demo/demo.vue
@@ -2,16 +2,16 @@
  * @Author: wanzp
  * @Date: 2022-10-31 21:20:45
  * @LastEditors: wanzp
- * @LastEditTime: 2023-05-05 20:42:36
+ * @LastEditTime: 2023-05-18 22:21:40
  * @Description: 
 -->
 <template>
   <div id="demo">
-    <input
+    <!-- <input
       type="text"
       v-model="filterText"
-      v-inputFilter:number="{ regExp: reg, allowSpace: false, allowChinese: false }"
-    />
+      v-inputFilter:text="{ regExp: reg, allowSpace: true, allowChinese: false }"
+    /> -->
 
     <input
       type="text"
@@ -38,15 +38,5 @@ const reg = new RegExp(String.raw`[${defaultStr}${characters}]`, 'g');
 function close() {
   visible.value = false;
 }
-
-function strSort() {
-  let list = ['Y831-03-10', 'J342-50-22', 'X200-02-01', 'J342-40-22', 'Y831-03-09'];
-  list = list?.sort();
-  console.log(list);
-}
-
-onMounted(() => {
-  strSort();
-});
 </script>
 <style lang="less" scoped></style>
