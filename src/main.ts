2fbe7b7b05cd47ad14b8df71d697035c8bc39f1b
wzp123321 <wanzhipengx@163.com>
2023-08-13 19:57:18 +0800

    修改安装源

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,3 @@
-/*
- * @Author: wanzp
- * @Date: 2022-07-11 19:35:59
- * @LastEditors: wanzp
-<<<<<<< HEAD
- * @LastEditTime: 2023-07-04 14:02:13
-=======
- * @LastEditTime: 2023-06-25 20:51:43
->>>>>>> 738aa385f13ae593329e3f8d1c8a58b1bcb79ebf
- * @Description:
- */
 import { createApp } from 'vue';
 import App from './App.vue';
 import { createPinia } from 'pinia';
@@ -21,10 +10,8 @@ import 'element-plus/dist/index.css';
 import 'jspreadsheet-ce/dist/jspreadsheet.css';
 import 'jsuites/dist/jsuites.css';
 
-import './assets/test-color.js'
+import './assets/test-color.js';
 
-// import registerInputFilter from './directives/directive-filter/directive-filter';
-// import dragDirectiveRegister from './directives/directive-drag/directive-drag';
 import registerInputFilter from './directives/inputFilter';
 
 const app = createApp(App);
