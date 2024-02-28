277e6f81434ec88aa9362b2e8e6f8888c4755981
wzp123321 <wanzhipengx@163.com>
2024-02-28 22:04:40 +0800

    新增代码

--- src/pages/echarts/chart-map/components/cm-bar.vue
+++ src/pages/echarts/chart-map/components/cm-bar.vue
@@ -19,53 +19,23 @@ const mapOptions = (): EChartsOption => {
         type: 'shadow',
       },
     },
-    legend: {
-      show: false,
-    },
     grid: {
-      left: '5%',
-      right: '5%',
-      top: '15%',
-      bottom: '30%',
+      left: '10%',
+      right: '8%',
+      top: '8%',
+      bottom: '24%',
     },
     xAxis: {
-      type: 'value',
-      name: 'Days',
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
       axisLabel: {
-        formatter: '{value}',
+        fontSize: 10,
       },
     },
     yAxis: {
-      type: 'category',
-      data: ['City Alpha1', 'City Beta2', 'City Gamma3', 'City Alpha4', 'City Beta5', 'City Gamma6'],
-      nameTextStyle: {
-        color: 'rgba(0, 0, 0, 0.65)',
-        fontSize: 14,
-        padding: [46, 0, 0, 0],
-      },
-      axisLine: {
-        lineStyle: {
-          color: 'rgba(0, 0, 0, 0.15)',
-        },
-      },
-      // 轴文本
+      type: 'value',
       axisLabel: {
-        show: true,
-        fontSize: 14,
-        color: 'rgba(255, 255, 255, 0.65)',
-        formatter: (name: string) => {
-          return format.truncateText(name, 100, '14px Microsoft Yahei', '…', {});
-        },
-      },
-      // 刻度
-      axisTick: {
-        show: false,
-      },
-      // 分割线
-      splitLine: {
-        lineStyle: {
-          color: 'rgba(0, 0, 0, 0.15)',
-        },
+        fontSize: 10,
       },
     },
     legend: {
@@ -79,46 +49,93 @@ const mapOptions = (): EChartsOption => {
       formatter: (name: string) => {
         return format.truncateText(name, 100, '14px Microsoft Yahei', '…', {});
       },
-      tooltip: {
-        show: true,
-      },
     },
     series: [
       {
-        name: 'City Alpha1',
+        name: 'Direct',
         type: 'bar',
-        stack: 'total',
-        data: [165, null, null, null, null, null],
+        emphasis: {
+          focus: 'series',
+        },
+        data: [320, 332, 301, 334, 390, 330, 320],
       },
       {
-        name: 'City Beta2',
+        name: 'Email',
         type: 'bar',
-        stack: 'total',
-        data: [null, 105, null, null, null, null],
+        stack: 'Ad',
+        emphasis: {
+          focus: 'series',
+        },
+        data: [120, 132, 101, 134, 90, 230, 210],
       },
       {
-        name: 'City Gamma3',
+        name: 'Union Ads',
         type: 'bar',
-        stack: 'total',
-        data: [null, null, 63, null, null, null],
+        stack: 'Ad',
+        emphasis: {
+          focus: 'series',
+        },
+        data: [220, 182, 191, 234, 290, 330, 310],
       },
       {
-        name: 'City Alpha4',
+        name: 'Video Ads',
         type: 'bar',
-        stack: 'total',
-        data: [null, null, null, 100, null, null],
+        stack: 'Ad',
+        emphasis: {
+          focus: 'series',
+        },
+        data: [150, 232, 201, 154, 190, 330, 410],
       },
       {
-        name: 'City Beta5',
+        name: 'Search Engine',
         type: 'bar',
-        stack: 'total',
-        data: [null, null, null, null, 120, null],
+        data: [862, 1018, 964, 1026, 1679, 1600, 1570],
+        emphasis: {
+          focus: 'series',
+        },
+        markLine: {
+          lineStyle: {
+            type: 'dashed',
+          },
+          data: [[{ type: 'min' }, { type: 'max' }]],
+        },
       },
       {
-        name: 'City Gamma6',
+        name: 'Baidu',
         type: 'bar',
-        stack: 'total',
-        data: [null, null, null, null, null, 144],
+        barWidth: 5,
+        stack: 'Search Engine',
+        emphasis: {
+          focus: 'series',
+        },
+        data: [620, 732, 701, 734, 1090, 1130, 1120],
+      },
+      {
+        name: 'Google',
+        type: 'bar',
+        stack: 'Search Engine',
+        emphasis: {
+          focus: 'series',
+        },
+        data: [120, 132, 101, 134, 290, 230, 220],
+      },
+      {
+        name: 'Bing',
+        type: 'bar',
+        stack: 'Search Engine',
+        emphasis: {
+          focus: 'series',
+        },
+        data: [60, 72, 71, 74, 190, 130, 110],
+      },
+      {
+        name: 'Others',
+        type: 'bar',
+        stack: 'Search Engine',
+        emphasis: {
+          focus: 'series',
+        },
+        data: [62, 82, 91, 84, 109, 110, 120],
       },
     ],
   };
--- src/pages/echarts/chart-map/components/cm-gauge.vue
+++ src/pages/echarts/chart-map/components/cm-gauge.vue
@@ -1,12 +1,95 @@
 <template>
-  <div class="cm-gauge">
-    cm-gauge
-  </div>
+  <div class="cm-gauge" ref="chartRef"></div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { onMounted, onUnmounted } from 'vue';
+import { useECharts } from '../../../../hook/useECharts';
+import { EChartsOption } from 'echarts';
+
+const { chartRef, addResize, removeResize, initCharts } = useECharts();
+/**
+ * 生成配置
+ */
+const mapOptions = (): EChartsOption => {
+  return {
+    grid: {
+      top: '0',
+      bottom: '0',
+      left: '0',
+      right: '0',
+    },
+    series: [
+      {
+        type: 'gauge',
+        axisLine: {
+          lineStyle: {
+            width: 30,
+            color: [
+              [0.3, '#67e0e3'],
+              [0.7, '#37a2da'],
+              [1, '#fd666d'],
+            ],
+          },
+        },
+        pointer: {
+          itemStyle: {
+            color: 'auto',
+          },
+        },
+        axisTick: {
+          distance: -30,
+          length: 8,
+          lineStyle: {
+            color: '#fff',
+            width: 2,
+          },
+        },
+        splitLine: {
+          distance: -30,
+          length: 30,
+          lineStyle: {
+            color: '#fff',
+            width: 4,
+          },
+        },
+        axisLabel: {
+          color: 'inherit',
+          distance: 40,
+          fontSize: 16,
+        },
+        detail: {
+          valueAnimation: true,
+          formatter: '{value} km/h',
+          color: 'inherit',
+        },
+        data: [
+          {
+            value: 70,
+          },
+        ],
+      },
+    ],
+  };
+};
+
+onMounted(() => {
+  initCharts(mapOptions());
+  addResize;
+});
+onUnmounted(() => {
+  removeResize;
+});
+</script>
 
 <style lang="less" scoped>
 .cm-gauge {
+  width: 100%;
+  height: 100%;
+
+  * {
+    width: 100%;
+    height: 100%;
+  }
 }
 </style>
