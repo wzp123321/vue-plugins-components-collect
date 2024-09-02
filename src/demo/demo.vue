2d1b3e585b347a28038460099ab95519d7563194
wzp123321 <wanzhipengx@163.com>
2024-09-02 21:50:14 +0800

    新增合并行拖拽

--- src/demo/demo.vue
+++ src/demo/demo.vue
@@ -4,8 +4,7 @@
       <el-table-column prop="id" label="ID" width="180" />
       <el-table-column type="sort" label="Name">
         <template #default="{ row, $index }">
-          <div :draggable="true" @dragstart="handleDragStart(row, $index)" @drop="handleDrop($event, row, $index)"
-            @dragover="handleDragOver($event, row, $index)">
+          <div :data-index="$index" @mousedown="handleMouseDown($event, row, $index)">
             {{ row.name }}
           </div>
         </template>
@@ -122,6 +121,30 @@ const tableData = ref<User[]>([
   },
 ]);
 
+let trNode: HTMLElement
+const handleMouseDown = (event: MouseEvent, row: User, index: number) => {
+  currentRow.id = row.id;
+  currentRow.name = row.name;
+  currentRow.amount1 = row.amount1;
+  currentRow.amount2 = row.amount2;
+  currentRow.amount3 = row.amount3;
+  currentIndex.value = index;
+
+  // https://deepinout.com/javascript/javascript-questions/195_77259549-how-to-change-cursor-while-dragging-in-dragstart-or-dragover-in-javascript.html#:~:text=draggableElement.addEventListener%28%27dragstart%27%2C%20function%28event%29%20%7B%20%2F%2F%20%E8%AE%BE%E7%BD%AE%E6%8B%96%E5%8A%A8%E6%97%B6%E7%9A%84%E5%85%89%E6%A0%87%E6%A0%B7%E5%BC%8F%E4%B8%BA%22move%22%20event.dataTransfer.effectAllowed%20%3D,%27move%27%3B%20%2F%2F%20%E4%BF%AE%E6%94%B9%E6%8B%96%E5%8A%A8%E6%97%B6%E6%98%BE%E7%A4%BA%E7%9A%84%E5%9B%BE%E6%A0%87%20event.dataTransfer.setDragImage%28draggableElement%2C%200%2C%200%29%3B%20%7D%29%3B
+  const target = event.target as HTMLElement
+  if (target) {
+    trNode = target.parentNode?.parentNode?.parentNode as HTMLElement
+    trNode.setAttribute('draggable', 'true')
+    trNode.addEventListener('dragstart', handleDragStart)
+    trNode.addEventListener('dragover', handleDragOver)
+    trNode.addEventListener('drop', handleDrop)
+    console.log(target, target.parentNode?.parentNode?.parentNode,);
+
+  }
+  // :draggable="true" @dragstart="handleDragStart(row, $index)" @drop="handleDrop($event, row, $index)"
+  // @dragover="handleDragOver($event, row, $index)"
+}
+
 const currentIndex = ref(-1);
 // 当前拖拽行
 const currentRow = reactive<User>({
@@ -133,55 +156,34 @@ const currentRow = reactive<User>({
   amount3: 0,
 });
 const dragFlag = ref(false);
-const handleDragStart = (row: User, index: number) => {
-  currentRow.id = row.id;
-  currentRow.name = row.name;
-  currentRow.amount1 = row.amount1;
-  currentRow.amount2 = row.amount2;
-  currentRow.amount3 = row.amount3;
+const handleDragStart = () => {
   dragFlag.value = true;
-  currentIndex.value = index;
-  console.log('handleDragStart-------------', index, row);
 };
 /**
  * drop 目标元素 被拖放的元素在目标元素上同时鼠标放开触发的事件
  * @param event 
- * @param row 
- * @param index 
  */
-const handleDrop = (event: Event, row: User, index: number) => {
-  if (row.id === currentRow.id) {
-    console.log('handleDrop -----------------', dragFlag.value, currentIndex.value, row, index);
-    const cloneItem = cloneDeep(currentRow);
-    if (index > currentIndex.value) {
-      tableData.value.splice(index + 1, 0, cloneItem);
-      tableData.value.splice(currentIndex.value, 1);
-    } else if (index < currentIndex.value) {
-      tableData.value.splice(index, 0, cloneItem);
-      console.log(tableData.value);
-      tableData.value.splice(currentIndex.value + 1, 1);
-      console.log(tableData.value);
-    }
+const handleDrop = (event: Event) => {
+  console.log('event-------------', event);
+  if (trNode) {
+    trNode.removeAttribute('draggable')
   }
-};
-const handleDragOver = (event: Event, row: User, index: number) => {
-  event.preventDefault()
-  // 只有同类的才可以拖拽
-  // if (row.id === currentRow.id && !dragFlag.value) {
-  //   console.log('handleDragOver -----------------', dragFlag.value, currentIndex.value, index);
+  // if (row.id === currentRow.id) {
+  //   console.log('handleDrop -----------------', dragFlag.value, currentIndex.value, row, index);
   //   const cloneItem = cloneDeep(currentRow);
   //   if (index > currentIndex.value) {
   //     tableData.value.splice(index + 1, 0, cloneItem);
   //     tableData.value.splice(currentIndex.value, 1);
   //   } else if (index < currentIndex.value) {
-  //     console.log(212);
-  //     tableData.value.splice(Math.max(index - 1, 0), 0, cloneItem);
+  //     tableData.value.splice(index, 0, cloneItem);
   //     console.log(tableData.value);
   //     tableData.value.splice(currentIndex.value + 1, 1);
   //     console.log(tableData.value);
   //   }
-  //   currentIndex.value = index
   // }
+};
+const handleDragOver = (event: Event) => {
+  event.preventDefault()
 }
 </script>
 <style lang="less" scoped>
